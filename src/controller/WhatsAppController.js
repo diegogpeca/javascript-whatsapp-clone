ee36d335380fe317e10e16fdcbd0dd605695d607
Diego Peça <diegogpeca@hotmail.com>
2019-09-06 18:31:37 -0300

    Gravando áudio capturado pelo microfone com MediaRecorder

--- src/controller/WhatsAppController.js
+++ src/controller/WhatsAppController.js
@@ -277,18 +277,20 @@ export class WhatsAppController {
             this.el.btnSendMicrophone.hide();
             this.startRecordMicrophoneTime();
             this._microphoneController = new MicrophoneController();
-            this._microphoneController.on('play', musica=>{
-                console.log('recebi o evento play', musica);
+            
+            this._microphoneController.on('ready', musica=>{
+                console.log('ready event');
+                this._microphoneController.startRecorder();
             });
         });
 
         this.el.btnCancelMicrophone.on('click', e=>{
-            this._microphoneController.stop();
+            this._microphoneController.stopRecorder();
             this.closeRecordMicrophone();
         });
 
         this.el.btnFinishMicrophone.on('click', e=>{
-            this._microphoneController.stop();
+            this._microphoneController.stopRecorder();
             this.closeRecordMicrophone();
         });
 

740a3ef12b56dc52e470a9213b40b911e155ddd8
Diego Peça <diegogpeca@hotmail.com>
2019-09-06 16:16:17 -0300

    Lendo capa de arquivo PDF e gerando imagem com PDF.js

--- src/controller/WhatsAppController.js
+++ src/controller/WhatsAppController.js
@@ -205,6 +205,11 @@ export class WhatsAppController {
 
         this.el.inputDocument.on('change', e=>{
             if (this.el.inputDocument.files.length){
+
+                this.el.panelDocumentPreview.css({
+                    'height':'1%'
+                });
+
                 let file = this.el.inputDocument.files[0];
 
                 this._documentPreviewController = new DocumentPreviewController(file);
@@ -214,9 +219,16 @@ export class WhatsAppController {
                     this.el.infoPanelDocumentPreview.innerHTML = result.info;
                     this.el.imagePanelDocumentPreview.show();
                     this.el.filePanelDocumentPreview.hide();
+                    this.el.panelDocumentPreview.css({
+                        'height':'calc(100% - 70px)'
+                    });
 
                 }).catch(err=>{
-                    console.log(file.type);
+
+                    this.el.panelDocumentPreview.css({
+                        'height':'calc(100% - 70px)'
+                    });
+
                     switch (file.type){
                         case 'application/vnd.ms-excel':
                         case 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet':

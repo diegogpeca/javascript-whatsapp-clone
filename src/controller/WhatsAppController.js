c14344a39a0357523cfbb99db25471fe295157b0
Diego Peça <diegogpeca@hotmail.com>
2019-09-06 18:52:02 -0300

    Criando evento para o visualizar tempo de gravação

--- src/controller/WhatsAppController.js
+++ src/controller/WhatsAppController.js
@@ -275,13 +275,16 @@ export class WhatsAppController {
         this.el.btnSendMicrophone.on('click', e=>{
             this.el.recordMicrophone.show();
             this.el.btnSendMicrophone.hide();
-            this.startRecordMicrophoneTime();
             this._microphoneController = new MicrophoneController();
             
             this._microphoneController.on('ready', musica=>{
                 console.log('ready event');
                 this._microphoneController.startRecorder();
             });
+
+            this._microphoneController.on('recordtimer', timer =>{
+                this.el.recordMicrophoneTimer.innerHTML = Format.toTime(timer);
+            })
         });
 
         this.el.btnCancelMicrophone.on('click', e=>{
@@ -349,19 +352,10 @@ export class WhatsAppController {
         });
     }
 
-    startRecordMicrophoneTime(){
-
-        let start = Date.now();
-        this._recordMicrophoneInterval = setInterval(()=>{
-            this.el.recordMicrophoneTimer.innerHTML = Format.toTime((Date.now() - start));
-        }, 100);
-    }
-
     closeRecordMicrophone(){
 
         this.el.recordMicrophone.hide();
-        this.el.btnSendMicrophone.show();
-        clearInterval(this._recordMicrophoneInterval);
+        this.el.btnSendMicrophone.show();        
     }
 
     closeAllMainPanel(){

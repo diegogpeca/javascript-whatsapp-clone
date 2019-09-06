cbcb24b4934fe4b9242e584c5a18ccff19e4b2bd
Diego Peça <diegogpeca@hotmail.com>
2019-09-06 00:39:45 -0300

    Tirar fotografia da câmera com canvas

--- src/controller/WhatsAppController.js
+++ src/controller/WhatsAppController.js
@@ -158,7 +158,7 @@ export class WhatsAppController {
             this.closeAllMainPanel();
             this.el.panelCamera.addClass('open');
             this.el.panelCamera.css({
-                'height':'calc(100% - 120px'
+                'height':'calc(100% - 50px)'
             });
 
             this._camera = new CameraController(this.el.videoCamera);
@@ -167,17 +167,36 @@ export class WhatsAppController {
         this.el.btnClosePanelCamera.on('click', e=>{
             this.closeAllMainPanel();
             this.el.panelMessagesContainer.show();
+            this._camera.stop();
         });
 
         this.el.btnTakePicture.on('click', e=>{
-            console.log('take picture');
+            let dataUrl = this._camera.takePicture();
+            this.el.pictureCamera.src = dataUrl;
+            this.el.pictureCamera.show();
+            this.el.videoCamera.hide();
+            this.el.btnReshootPanelCamera.show();
+            this.el.containerTakePicture.hide();
+            this.el.containerSendPicture.show();
         });
+
+        this.el.btnReshootPanelCamera.on('click', e=>{
+            this.el.pictureCamera.hide();
+            this.el.videoCamera.show();
+            this.el.btnReshootPanelCamera.hide();
+            this.el.containerTakePicture.show();
+            this.el.containerSendPicture.hide();
+        });
+
+        this.el.btnSendPicture.on('click', e=>{
+            console.log(this.el.pictureCamera.src);
+        })
 
         this.el.btnAttachDocument.on('click', e=>{
             this.closeAllMainPanel();
             this.el.panelDocumentPreview.addClass('open');
             this.el.panelDocumentPreview.css({
-                'height':'calc(100% - 120px'
+                'height':'calc(100% - 120px)'
             });
         }); 
         

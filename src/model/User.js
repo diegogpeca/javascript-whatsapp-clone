2381c40b81572975cb4dfaeb6e2bfc5a05ee113f
Diego Peça <diegogpeca@hotmail.com>
2019-09-13 20:22:03 -0300

    Listando os contatos

--- src/model/User.js
+++ src/model/User.js
@@ -39,6 +39,11 @@ export class User extends Model{
         return Firebase.db().collection('/users');
     }
 
+    static getContactsRef(id){
+
+        return User.getRef().doc(id).collection('contacts');
+    }
+
     static findByEmail(email){
 
         return User.getRef().doc(email);
@@ -46,6 +51,26 @@ export class User extends Model{
 
     addContact(contact){
 
-       return User.getRef().doc(this.email).collection('contacts').doc(btoa(contact.email)).set(contact.toJSON());
+       return User.getContactsRef(this.email).doc(this.email).collection('contacts').doc(btoa(contact.email)).set(contact.toJSON());
+    }
+
+    getContacts(){
+
+        return new Promise((s, f)=>{
+
+            User.getContactsRef(this.email).onSnapshot(docs =>{
+                let contacts = [];
+                docs.forEach(doc =>{
+                    let data = doc.data();
+                    data.id = doc.id;
+                    contacts.push(data);
+                });
+
+                this.trigger('contactschange', docs);
+                s(contacts);
+
+            });
+
+        });
     }
 }
